8b796fc8ae2cc384593373d7248d054ac672572c
Valso <warut92@gmail.com>
2022-12-25 19:46:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function letSmile() {
+  var emoji = document.getElementById("emoji");
+  if ((emoji = "&#128551;")) {
+    // console.log(emoji);
+    document.getElementById("emoji").innerHTML = "&#128513;" + "<br> ยิ้มแย้วว อิ้อิ้";
+    var changeEmoji = setTimeout(painedEmoji, 5000);
+  }
+}
+
+function painedEmoji() {
+  document.getElementById("emoji").innerHTML = "&#128551;" + "<br> วันนี้คุณยิ้มแล้วหรือยัง";
+}
